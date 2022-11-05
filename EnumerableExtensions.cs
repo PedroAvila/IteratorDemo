f035841a6a65f41d7fd0997b663b200ce064482b
Pedro Ulises Ávila Núñez <minimainor_austin@hotmail.com>
2022-11-05 17:54:01 -0500

    Agregar archivos de proyecto.

---
+++ EnumerableExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorDemo
+{
+    public static class EnumerableExtensions
+    {
+        public static T WithMinimun<T, TKey>(this IEnumerable<T> sequence, Func<T, TKey> criterion)
+            where T : class
+            where TKey : IComparable<TKey> =>
+                sequence
+                    .Select(obj => Tuple.Create(obj, criterion(obj)))
+                    .Aggregate((Tuple<T, TKey>)null,
+                        (best, cur) => best == null || cur.Item2.CompareTo(best.Item2) < 0 ? cur : best)
+            .Item1;
+
+           // sequence.Aggregate((T)null, (best, cur) =>
+           //best == null || criterion(cur).CompareTo(criterion(best)) < 0 ? cur : best);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorDemo
+{
+    class Program
+    {
+        private static IPainter FindCheapestPainter(double sqMeters, IEnumerable<IPainter> painters)
+        {
+            return
+                painters
+                    .Where(painter => painter.IsAvailable)
+                    .WithMinimun(painter => painter.EstimateCompensation(sqMeters));
+                    
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
